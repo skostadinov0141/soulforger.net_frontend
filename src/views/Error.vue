791b52766f5731863dbf1790c85ad302b3a10b64
skostadinov0141 <simeon.kostadinov0141@gmail.com>
2023-09-20 11:36:57 +0200

    started updating error view

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,19 +1,31 @@
 <template>
-	<p style="white-space: pre-wrap">
-		{{ JSON.stringify(error, null, 4) }}
-	</p>
+	<v-container class="h-100">
+		<v-row align="center" class="h-100" no-gutters>
+			<v-col cols="12" md="6" offset-md="3">
+				<v-card color="surface-lighten-1">
+					<v-card-title class="text-error">Ein Fehler ist aufgetreten!</v-card-title>
+					<v-card-text>
+						{{ `${error.config?.baseURL}${error.config?.url}` }}
+					</v-card-text>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script setup lang="ts">
 import { AxiosError } from "axios";
+import { onMounted } from "vue";
 import { ref } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 
-const error = ref<AxiosError>(
-	new AxiosError(JSON.parse(route.query.error as string))
-);
+const error = ref<AxiosError>(new AxiosError());
+
+onMounted(() => {
+	error.value = JSON.parse(route.query.error as string);
+});
 </script>
 
 <style scoped></style>
