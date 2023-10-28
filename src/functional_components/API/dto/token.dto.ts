7fae5a7e339f23af67a5dc62c7c4f3dfeba1f698
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2023-10-28 03:02:33 +0200

    added API base class that takes care of Token management

---
+++ src/functional_components/API/dto/token.dto.ts
@@ -0,0 +1,6 @@
+export class TokenDto {
+  access_token: string;
+  refresh_token: string;
+  token_type: string;
+  expires_at: number;
+}
---
+++ src/functional_components/API/dto/update-user.dto.ts
@@ -0,0 +1,6 @@
+export class UpdateUserDto {
+  email?: string;
+  passwordHash?: string;
+  username?: string;
+  roles?: string[];
+}
