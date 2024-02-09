f34a1e00e39a01d863a8d8a772fe4b18b225aefa
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-02-09 18:57:32 +0100

    Added validation to tag and group management; added QoL features to tag and group management

--- src/views/nirve/ManageGroups.vue
+++ src/views/nirve/ManageGroups.vue
@@ -3,79 +3,92 @@
   <v-container>
     <v-dialog width="500" v-model="dialogOpen">
       <v-sheet class="pa-4">
-        <v-row no-gutters>
-          <v-col cols="12" class="d-flex align-center">
-            <v-text-field
-              hide-details
-              variant="solo-filled"
-              density="comfortable"
-              label="Gruppe erstellen"
-              class="ma-2"
-              v-model="editingGroup!.name"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="12" class="d-flex align-center">
-            <v-textarea
-              :no-resize="true"
-              rows="5"
-              hide-details
-              variant="solo-filled"
-              density="comfortable"
-              label="Beschreibung (optional)"
-              class="ma-2"
-              v-model="editingGroup!.description"
-            ></v-textarea>
-          </v-col>
-          <v-col class="ma-2">
-            <v-btn color="primary" @click="editGroup" :block="true"
-              >Speichern</v-btn
-            >
-          </v-col>
-        </v-row>
+        <v-form v-model="editValid">
+          <v-row no-gutters>
+            <v-col cols="12" class="d-flex align-center">
+              <v-text-field
+                  @keydown.enter.prevent="editGroup"
+                  hint="Drücke Enter zum Speichern"
+                  hide-details="auto"
+                  variant="solo-filled"
+                  density="comfortable"
+                  label="Gruppenname"
+                  class="ma-2"
+                  :rules="[required]"
+                  v-model="editingGroup!.name"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" class="d-flex align-center">
+              <v-textarea
+                  :no-resize="true"
+                  rows="5"
+                  hide-details="auto"
+                  hint="Feld ist optional"
+                  variant="solo-filled"
+                  density="comfortable"
+                  label="Beschreibung"
+                  class="ma-2"
+                  v-model="editingGroup!.description"
+              ></v-textarea>
+            </v-col>
+            <v-col class="ma-2">
+              <v-btn color="primary" @click="editGroup" :block="true"
+              >Speichern
+              </v-btn
+              >
+            </v-col>
+          </v-row>
+        </v-form>
       </v-sheet>
     </v-dialog>
     <v-sheet color="surface pa-2 mb-4 mt-8" rounded>
-      <v-row no-gutters>
-        <v-col cols="12" class="d-flex align-center">
-          <v-text-field
-            hide-details
-            variant="solo-filled"
-            density="comfortable"
-            label="Gruppe erstellen"
-            class="ma-2"
-            v-model="group.name"
-          ></v-text-field>
-          <v-btn
-            rounded
-            height="50"
-            class="ma-2"
-            icon="mdi-plus"
-            color="primary"
-            @click="createGroup"
-          >
-          </v-btn>
-        </v-col>
-        <v-col cols="12" class="d-flex align-center">
-          <v-textarea
-            :no-resize="true"
-            rows="5"
-            hide-details
-            variant="solo-filled"
-            density="comfortable"
-            label="Beschreibung (optional)"
-            class="ma-2"
-            v-model="group.description"
-          ></v-textarea>
-        </v-col>
-      </v-row>
+      <v-form v-model="createValid" @submit.prevent="createGroup">
+        <v-row no-gutters>
+          <v-col cols="12" class="d-flex align-start">
+            <v-text-field
+                hint="Drücke Enter zum Erstellen"
+                hide-details="auto"
+                @keydown.enter.prevent="createGroup"
+                variant="solo-filled"
+                density="comfortable"
+                label="Gruppenname"
+                class="ma-2"
+                v-model="group.name"
+                :rules="[required]"
+            ></v-text-field>
+            <v-btn
+                rounded
+                type="submit"
+                height="50"
+                class="ma-2"
+                icon="mdi-plus"
+                color="primary"
+            >
+            </v-btn>
+          </v-col>
+          <v-col cols="12" class="d-flex align-center">
+            <v-textarea
+                hint="Feld ist optional"
+                :no-resize="true"
+                rows="5"
+                hide-details="auto"
+                variant="solo-filled"
+                density="comfortable"
+                label="Beschreibung"
+                class="ma-2"
+                v-model="group.description"
+            ></v-textarea>
+          </v-col>
+        </v-row>
+      </v-form>
     </v-sheet>
     <v-sheet color="surface mb-4 d-flex align-center" rounded>
       <v-data-table
-        :headers="headers"
-        :items="groups"
-        :items-per-page="20"
-        :items-per-page-options="[]"
-        class="elevation-1"
+          :headers="headers"
+          :items="groups"
+          :items-per-page="20"
+          :items-per-page-options="[]"
+          class="elevation-1"
       >
         <template v-slot:item.createdAt="{ item }">
           <span>{{ new Date(item.createdAt).toLocaleString("DE-de") }}</span>
@@ -97,11 +110,12 @@
 </template>
 
 <script setup lang="ts">
-import { useApiStore } from "@/store/api";
-import { onMounted, ref } from "vue";
-import { NirveGroup } from "@/functional_components/API/nirve-group/nirve-group.class";
-import { VDataTable } from "vuetify/labs/VDataTable";
-import { CreateNirveGroupDto } from "@/functional_components/API/nirve-group/dto/create-nirve-group.dto";
+import {useApiStore} from "@/store/api";
+import {onMounted, ref} from "vue";
+import {NirveGroup} from "@/functional_components/API/nirve-group/nirve-group.class";
+import {VDataTable} from "vuetify/labs/VDataTable";
+import {CreateNirveGroupDto} from "@/functional_components/API/nirve-group/dto/create-nirve-group.dto";
+import {required} from "@/validators";
 
 const apiStore = useApiStore();
 
@@ -112,79 +126,82 @@ const group = ref<CreateNirveGroupDto>({
 });
 const dialogOpen = ref(false);
 const editingGroup = ref<NirveGroup | null>(null);
+const createValid = ref(false);
+const editValid = ref(false);
 
 const headers = ref([
-  { title: "Gruppe", value: "name" },
-  { title: "Erstellt am", value: "createdAt" },
-  { title: "Bearbeitet am", value: "updatedAt" },
-  { title: "Aktionen", value: "actions", sortable: false },
+  {title: "Gruppe", value: "name"},
+  {title: "Erstellt am", value: "createdAt"},
+  {title: "Bearbeitet am", value: "updatedAt"},
+  {title: "Aktionen", value: "actions", sortable: false},
 ]);
 
 onMounted(() => {
   apiStore.api.nirveGroupService
-    .search()
-    .then((tagsRes) => {
-      groups.value = tagsRes;
-    })
-    .catch(() => {
-      console.log("Error");
-    });
-});
-
-function createGroup() {
-  apiStore.api.nirveGroupService
-    .post(group.value)
-    .then((tag) => {
-      group.value = { name: "", description: "" };
-      refreshGroups();
-    })
-    .catch(() => {
-      console.log("Error");
-    });
-}
-
-function refreshGroups() {
-  apiStore.api.nirveGroupService
-    .search()
-    .then((tagsRes) => {
-      groups.value = tagsRes;
-    })
-    .catch(() => {
-      console.log("Error");
-    });
-}
-
-function deleteGroup(group: NirveGroup) {
-  apiStore.api.nirveGroupService
-    .deleteById(group._id)
-    .then(() => {
-      refreshGroups();
-    })
-    .catch(() => {
-      console.log("Error");
-    });
-}
-
-function openEditDialog(group: NirveGroup) {
-  editingGroup.value = { ...group };
-  dialogOpen.value = true;
-}
-
-function editGroup() {
-  console.log(editingGroup.value);
-  if (editingGroup.value) {
-    apiStore.api.nirveGroupService
-      .patch(editingGroup.value._id, {
-        name: editingGroup.value.name,
-        description: editingGroup.value.description,
-      })
-      .then(() => {
-        refreshGroups();
-        dialogOpen.value = false;
+      .search()
+      .then((tagsRes) => {
+        groups.value = tagsRes;
       })
       .catch(() => {
         console.log("Error");
       });
+});
+
+function createGroup() {
+  if (!createValid.value) return;
+  apiStore.api.nirveGroupService
+      .post(group.value)
+      .then((tag) => {
+        group.value = {name: "", description: ""};
+        refreshGroups();
+      })
+      .catch(() => {
+        console.log("Error");
+      });
+}
+
+function refreshGroups() {
+  apiStore.api.nirveGroupService
+      .search()
+      .then((tagsRes) => {
+        groups.value = tagsRes;
+      })
+      .catch(() => {
+        console.log("Error");
+      });
+}
+
+function deleteGroup(group: NirveGroup) {
+  apiStore.api.nirveGroupService
+      .deleteById(group._id)
+      .then(() => {
+        refreshGroups();
+      })
+      .catch(() => {
+        console.log("Error");
+      });
+}
+
+function openEditDialog(group: NirveGroup) {
+  editingGroup.value = {...group};
+  dialogOpen.value = true;
+}
+
+function editGroup() {
+  if (!editValid.value) return;
+  if (editingGroup.value) {
+    apiStore.api.nirveGroupService
+        .patch(editingGroup.value._id, {
+          name: editingGroup.value.name,
+          description: editingGroup.value.description,
+        })
+        .then(() => {
+          refreshGroups();
+          dialogOpen.value = false;
+        })
+        .catch(() => {
+          console.log("Error");
+        });
   }
 }
 </script>
--- src/views/nirve/ManageTags.vue
+++ src/views/nirve/ManageTags.vue
@@ -3,43 +3,52 @@
   <v-container>
     <v-dialog width="500" v-model="dialogOpen">
       <v-sheet class="pa-4">
-        <v-text-field
-          hide-details
-          variant="solo-filled"
-          density="comfortable"
-          label="Tag bearbeiten"
-          v-model="editTagName"
-          class="mb-2"
-        ></v-text-field>
-        <v-btn color="primary" @click="editTag" :block="true">Speichern</v-btn>
+        <v-form v-model="editValid" @submit.prevent="editTag">
+          <v-text-field
+              hide-details="auto"
+              hint="Drücke Enter zum Speichern"
+              variant="solo-filled"
+              density="comfortable"
+              label="Tag Name bearbeiten"
+              v-model="editTagName"
+              class="mb-2"
+              :rules="[required]"
+          ></v-text-field>
+          <v-btn color="primary" :block="true">Speichern</v-btn>
+        </v-form>
       </v-sheet>
     </v-dialog>
-    <v-sheet color="surface pa-2 mb-4 mt-8 d-flex align-center" rounded>
-      <v-text-field
-        hide-details
-        variant="solo-filled"
-        density="comfortable"
-        label="Tag erstellen"
-        class="ma-2"
-        v-model="tagName"
-      ></v-text-field>
-      <v-btn
-        rounded
-        height="50"
-        class="ma-2"
-        icon="mdi-plus"
-        color="primary"
-        @click="createTag"
-      >
-      </v-btn>
+    <v-sheet color="surface pa-2 mb-4 mt-8" rounded>
+      <v-form v-model="createValid" class="d-flex align-start" @submit.prevent="createTag">
+        <v-text-field
+            @keydown.enter.prevent="createTag"
+            hide-details="auto"
+            hint="Drücke Enter zum Erstellen"
+            variant="solo-filled"
+            density="comfortable"
+            label="Tag Name"
+            class="ma-2"
+            v-model="tagName"
+            :rules="[required]"
+        ></v-text-field>
+        <v-btn
+            type="submit"
+            rounded
+            height="50"
+            class="ma-2"
+            icon="mdi-plus"
+            color="primary"
+        >
+        </v-btn>
+      </v-form>
     </v-sheet>
     <v-sheet color="surface mb-4 d-flex align-center" rounded>
       <v-data-table
-        :headers="headers"
-        :items="tags"
-        :items-per-page="20"
-        :items-per-page-options="[]"
-        class="elevation-1"
+          :headers="headers"
+          :items="tags"
+          :items-per-page="20"
+          :items-per-page-options="[]"
+          class="elevation-1"
       >
         <template v-slot:item.createdAt="{ item }">
           <span>{{ new Date(item.createdAt).toLocaleString("DE-de") }}</span>
@@ -61,10 +70,11 @@
 </template>
 
 <script setup lang="ts">
-import { useApiStore } from "@/store/api";
-import { onMounted, ref } from "vue";
-import { NirveTag } from "@/functional_components/API/nirve-tag/nirve-tag.class";
-import { VDataTable } from "vuetify/labs/VDataTable";
+import {useApiStore} from "@/store/api";
+import {onMounted, ref} from "vue";
+import {NirveTag} from "@/functional_components/API/nirve-tag/nirve-tag.class";
+import {VDataTable} from "vuetify/labs/VDataTable";
+import {required} from "@/validators";
 
 const apiStore = useApiStore();
 
@@ -73,56 +83,59 @@ const tagName = ref<string>("");
 const dialogOpen = ref(false);
 const editingTag = ref<NirveTag | null>(null);
 const editTagName = ref<string>("");
+const createValid = ref(false);
+const editValid = ref(false);
 
 const headers = ref([
-  { title: "Tag", value: "tag" },
-  { title: "Erstellt am", value: "createdAt" },
-  { title: "Bearbeitet am", value: "updatedAt" },
-  { title: "Aktionen", value: "actions", sortable: false },
+  {title: "Tag", value: "tag"},
+  {title: "Erstellt am", value: "createdAt"},
+  {title: "Bearbeitet am", value: "updatedAt"},
+  {title: "Aktionen", value: "actions", sortable: false},
 ]);
 
 onMounted(() => {
   apiStore.api.nirveTagService
-    .search()
-    .then((tagsRes) => {
-      tags.value = tagsRes;
-    })
-    .catch(() => {
-      console.log("Error");
-    });
+      .search()
+      .then((tagsRes) => {
+        tags.value = tagsRes;
+      })
+      .catch(() => {
+        console.log("Error");
+      });
 });
 
 function createTag() {
+  if (!createValid.value) return;
   apiStore.api.nirveTagService
-    .post({ tag: tagName.value })
-    .then((tag) => {
-      refreshTags();
-    })
-    .catch(() => {
-      console.log("Error");
-    });
+      .post({tag: tagName.value})
+      .then((tag) => {
+        refreshTags();
+      })
+      .catch(() => {
+        console.log("Error");
+      });
 }
 
 function refreshTags() {
   apiStore.api.nirveTagService
-    .search()
-    .then((tagsRes) => {
-      tags.value = tagsRes;
-    })
-    .catch(() => {
-      console.log("Error");
-    });
+      .search()
+      .then((tagsRes) => {
+        tags.value = tagsRes;
+      })
+      .catch(() => {
+        console.log("Error");
+      });
 }
 
 function deleteTag(tag: NirveTag) {
   apiStore.api.nirveTagService
-    .deleteById(tag._id)
-    .then(() => {
-      refreshTags();
-    })
-    .catch(() => {
-      console.log("Error");
-    });
+      .deleteById(tag._id)
+      .then(() => {
+        refreshTags();
+      })
+      .catch(() => {
+        console.log("Error");
+      });
 }
 
 function openEditDialog(tag: NirveTag) {
@@ -132,16 +145,17 @@ function openEditDialog(tag: NirveTag) {
 }
 
 function editTag() {
+  if (!editValid.value) return;
   if (editingTag.value) {
     apiStore.api.nirveTagService
-      .patch(editingTag.value._id, { tag: editTagName.value })
-      .then(() => {
-        refreshTags();
-        dialogOpen.value = false;
-      })
-      .catch(() => {
-        console.log("Error");
-      });
+        .patch(editingTag.value._id, {tag: editTagName.value})
+        .then(() => {
+          refreshTags();
+          dialogOpen.value = false;
+        })
+        .catch(() => {
+          console.log("Error");
+        });
   }
 }
 </script>
